ca21f36c161e49ee2c64fc6a2bba638ddeffaae0
Grégory PINON <elsopeen@gmail.com>
2021-11-03 10:52:18 +0100

    Grabbing tested
    Added grabbable ball
    HandPresence cannot be used as HTC controllers aren't detected correctly

--- Assets/HandPresence.cs
+++ Assets/HandPresence.cs
@@ -5,17 +5,26 @@ using UnityEngine.XR;
 
 public class HandPresence : MonoBehaviour
 {
+    private InputDevice targetDevice;
+    private GameObject spawnedController;
+    public InputDeviceCharacteristics controllerCharacteristics;
+    public List<GameObject> controllerPrefabs;
     // Start is called before the first frame update
     void Start()
     {
         List<InputDevice> devices = new List<InputDevice>();
-
-        InputDeviceCharacteristics rightControllerCharacteristics = InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller;
-        InputDevices.GetDevicesWithCharacteristics(rightControllerCharacteristics, devices);
-
+        InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics, devices);
         foreach (var item in devices)
         {
+            Debug.Log(item);
             Debug.Log(item.name + item.characteristics);
+        }
+        if(devices.Count > 0)
+        {
+            targetDevice = devices[0];
+            GameObject prefab = controllerPrefabs[1];
+            if (prefab)
+                spawnedController = Instantiate(prefab, transform);
         }
 
     }
