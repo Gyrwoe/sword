0f1b62744dca1fdee49b0a496a304719af590bf5
Grégory PINON <elsopeen@gmail.com>
2021-11-03 12:23:43 +0100

    Started merging
    Fucked up materials, sry

---
+++ Assets/Scripts/HideRay.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class HideRay : MonoBehaviour
+{
+    public XRController rightTPController;
+    public XRController leftTPController;
+    private XRInteractorLineVisual rightTeleportRay;
+    private XRInteractorLineVisual leftTeleportRay;
+    public InputHelpers.Button teleportActivationButton;
+    public float activationThreshold = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rightTeleportRay = rightTPController.GetComponent(typeof(XRInteractorLineVisual)) as XRInteractorLineVisual;
+        leftTeleportRay = leftTPController.GetComponent(typeof(XRInteractorLineVisual)) as XRInteractorLineVisual;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rightTeleportRay)
+        {
+            rightTeleportRay.enabled = CheckIfActivated(rightTPController);
+        }
+        if (leftTeleportRay)
+        {
+            leftTeleportRay.enabled = CheckIfActivated(leftTPController);
+        }
+    }
+
+    public bool CheckIfActivated(XRController ray)
+    {
+        InputHelpers.IsPressed(ray.inputDevice, teleportActivationButton, out bool isActivated, activationThreshold);
+        return isActivated;
+    }
+}
