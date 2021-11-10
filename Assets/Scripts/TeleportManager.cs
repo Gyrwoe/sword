dfc840e46211290636d0fb512f3705ae71d34010
Grégory PINON <elsopeen@gmail.com>
2021-11-10 11:36:24 +0100

    Commiting progress on own tp manager to use trigger button and not grip

---
+++ Assets/Scripts/TeleportManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class TeleportManager : MonoBehaviour
+{
+    [SerializeField] private InputActionAsset actionAsset;
+    private InputAction _thumbstick;
+    void Start()
+    {
+        var activate = actionAsset.FindActionMap("XRI RightHand").FindAction("Teleportation Mode Activate");
+        activate.Enable();
+        activate.performed += OnTeleportActivate;
+
+        var cancel = actionAsset.FindActionMap("XRI RightHand").FindAction("Teleportation Mode Cancel");
+        cancel.Enable();
+        cancel.performed += OnTeleportCancel;
+
+        _thumbstick = actionAsset.FindActionMap("XRI RightHand").FindAction("Move");
+        _thumbstick.Enable();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTeleportActivate(InputAction.CallbackContext context)
+    {
+
+    }
+
+    private void OnTeleportCancel(InputAction.CallbackContext context)
+    {
+
+    }
+}
